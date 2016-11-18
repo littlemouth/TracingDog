d5528dd23a34c8e0e905a9e7182a644a1daacec6
yanfei <yanfei@supcon.com>
2016-11-18 16:52:56 +0800

    save

--- app/src/main/java/dfst/com/tracingdog/fragment/TracingListFragment.java
+++ app/src/main/java/dfst/com/tracingdog/fragment/TracingListFragment.java
@@ -6,16 +6,54 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.IOException;
 
 import dfst.com.trackingdog.R;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by yanfei on 2016-10-25.
  */
 public class TracingListFragment extends TabFragment {
+    private Button button;
+    private OkHttpClient client = new OkHttpClient();
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tracing_list, null);
+        View root = inflater.inflate(R.layout.fragment_tracing_list, null);
+        button = (Button) root.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread() {
+                    public void run() {
+                        try {
+                            post("http://192.168.42.53:8080/historylocation/1", "{\"userId\": 123}");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+            }
+        });
+        return root;
+    }
+
+    private String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 }
