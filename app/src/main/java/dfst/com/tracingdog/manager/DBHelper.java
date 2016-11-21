57c21c8dc27ccefd42e8e55c0245c1346705bc66
yanfei <yanfei@supcon.com>
2016-11-21 17:02:46 +0800

    save

---
+++ app/src/main/java/dfst/com/tracingdog/manager/DBHelper.java
@@ -0,0 +1,35 @@
+package dfst.com.tracingdog.manager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by yanfei on 2016-11-21.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+    private final static String DB_NAME ="tracingdog.db";//数据库名
+    private final static int VERSION = 1;//版本号
+
+    private SQLiteDatabase database;
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        createTable();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion != newVersion)
+            createTable();
+    }
+
+    private void createTable() {
+
+    }
+
+}
